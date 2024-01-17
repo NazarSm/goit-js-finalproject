c4683e009b071bb992f1086bacb0707c2d86fd21
Nazar Smutnitskiy <nsmutnitskiy@swivl.com>
2024-01-17 22:57:38 +0200

    add final project

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems
+    .map((imageInfo) => `<li class="gallery__item">
+  <a class="gallery__link" href="${imageInfo.original}" >
+    <img
+      class="gallery__image"
+      src="${imageInfo.preview}"
+      data-source="${imageInfo.original}"
+      alt="${imageInfo.description}"
+    />
+  </a>
+</li>`)
+    .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+const galleryLinks = document.querySelectorAll('.gallery__link');
+
+const handleClick = (event) => {
+    const targetEvent = event.target;
+
+    if (targetEvent.nodeName !== 'IMG') {
+        return;
+    }
+
+    const instance = basicLightbox.create(`<img src="${targetEvent.dataset.source}" alt="${targetEvent.alt}">`)
+
+    instance.show();
+};
+
+const handleClickLink = (event) => {
+    event.preventDefault();
+}
+
+galleryLinks.forEach(galleryLink => {
+    galleryLink.addEventListener('click', handleClickLink);
+});
+
+gallery.addEventListener('click', handleClick);
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems
+    .map((imageInfo) => `<li class="gallery__item">
+   <a class="gallery__link" href="${imageInfo.original}">
+      <img class="gallery__image" src="${imageInfo.preview}" alt="${imageInfo.description}" />
+   </a>
+</li>`)
+    .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+const handleClickLink = (event) => {
+    event.preventDefault();
+}
+
+const galleryLinks = document.querySelectorAll('.gallery__link');
+
+galleryLinks.forEach(galleryLink => {
+    galleryLink.addEventListener('click', handleClickLink);
+});
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250
+});
